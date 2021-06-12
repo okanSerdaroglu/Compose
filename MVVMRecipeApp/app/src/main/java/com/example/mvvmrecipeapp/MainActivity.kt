ea12519eaa84ae537f07b5ab3ff2713e0d5a51a0
okanSerdaroglu <okanserdaroglu2@gmail.com>
2021-06-12 23:27:35 +0300

    jetpack compose setup for android studio

--- MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.mvvmrecipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            showButton()
+        }
+    }
+
+    @Preview("initialComposable")
+    @Composable
+    fun showButton() {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Some Text")
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Button(
+                onClick = {}
+            ) {
+                Text(text = "A Button")
+            }
+        }
     }
 }
