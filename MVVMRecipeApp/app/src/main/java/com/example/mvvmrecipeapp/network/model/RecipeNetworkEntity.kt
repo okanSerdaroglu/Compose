69e8f55bcafa7e7907edf08423201baa37a813b4
okanSerdaroglu <okanserdaroglu2@gmail.com>
2021-06-13 21:43:54 +0300

    recipe network entity ( retrofit model )

---
+++ MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/network/model/RecipeNetworkEntity.kt
@@ -0,0 +1,41 @@
+package com.example.mvvmrecipeapp.network.model
+
+import com.google.gson.annotations.SerializedName
+
+class RecipeNetworkEntity(
+    @SerializedName("pk")
+    var pk: Int? = null,
+
+    @SerializedName("title")
+    val title: String? = null,
+
+    @SerializedName("publisher")
+    var publisher: String? = null,
+
+    @SerializedName("rating")
+    var rating: Int? = null,
+
+    @SerializedName("source_url")
+    var sourceUrl: String? = null,
+
+    @SerializedName("featured_image")
+    var featured_image: String? = null,
+
+    @SerializedName("description")
+    var description: String? = null,
+
+    @SerializedName("cooking_instructions")
+    var cookingInstructions: String? = null,
+
+    @SerializedName("ingredients")
+    var ingredients: List<String>? = null,
+
+    @SerializedName("date_added")
+    var dateAdded: String? = null,
+
+    @SerializedName("date_updated")
+    var dateUpdated: String? = null
+)
+
+
+
