d1160c1599081e0ce5d422fe274256b0ff7b003c
okanSerdaroglu <okanserdaroglu2@gmail.com>
2021-06-15 21:36:27 +0300

    hilt dependency injection

---
+++ MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.example.mvvmrecipeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
--- MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.mvvmrecipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var application: BaseApplication
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("hilt ", application.toString())
     }
 }
