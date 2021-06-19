46de7d96435231946107a9d68bf31668a2fe98cc
okanSerdaroglu <okanserdaroglu2@gmail.com>
2021-06-20 00:05:54 +0300

    hilt - providing retrofit instance and repository

---
+++ MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/di/RepositoryModule.kt
@@ -0,0 +1,27 @@
+package com.example.mvvmrecipeapp.di
+
+import com.example.mvvmrecipeapp.network.RecipeService
+import com.example.mvvmrecipeapp.network.model.RecipeDtoMapper
+import com.example.mvvmrecipeapp.repository.RecipeRepository
+import com.example.mvvmrecipeapp.repository.RecipeRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Singleton
+    @Provides
+    fun provideRepository(
+        recipeService: RecipeService,
+        recipeDtoMapper: RecipeDtoMapper
+    ): RecipeRepository {
+        return RecipeRepositoryImpl(
+            recipeService = recipeService,
+            mapper = recipeDtoMapper
+        )
+    }
+}
--- MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -1,12 +1,17 @@
 package com.example.mvvmrecipeapp.presentation.ui.recipe_list
 
 import androidx.lifecycle.ViewModel
+import com.example.mvvmrecipeapp.repository.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import javax.inject.Named
 
 @HiltViewModel
 class RecipeListViewModel
+@Inject
 constructor(
-
+    private val repository: RecipeRepository,
+    @Named("auth_token") private val token: String
 ) : ViewModel() {
 
 }
