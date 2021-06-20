dc60f5ea6dec32b09827755ee663a7d0b93fedef
okanSerdaroglu <okanserdaroglu2@gmail.com>
2021-06-20 23:17:28 +0300

    searchView toolbar with jetpack compose

--- MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ MVVMRecipeApp/app/src/main/java/com/example/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,31 +1,27 @@
 package com.example.mvvmrecipeapp.presentation.ui.recipe_list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
-import com.example.mvvmrecipeapp.R
 import com.example.mvvmrecipeapp.presentation.components.RecipeCard
-import com.example.mvvmrecipeapp.util.Constants.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -50,12 +46,48 @@ class RecipeListFragment : Fragment() {
                 val query = viewModel.query.value // for configuration change
 
                 Column {
-                    TextField(value = query,
-                        onValueChange = { newValue ->
-                            viewModel.onQueryChanged(newValue)
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        color = MaterialTheme.colors.primary,
+                        elevation = 8.dp,
+                    ) {
+                        Column {
+                            Row(modifier = Modifier.fillMaxWidth()) {
+                                TextField(
+                                    modifier = Modifier
+                                        .fillMaxWidth(0.9f)
+                                        .padding(8.dp),
+                                    value = query,
+                                    onValueChange = { newValue ->
+                                        viewModel.onQueryChanged(newValue)
+                                    },
+                                    label = {
+                                        Text(text = "search")
+                                    },
+                                    keyboardOptions = KeyboardOptions(
+                                        keyboardType = KeyboardType.Text,
+                                        imeAction = ImeAction.Search,
+                                    ),
+                                    keyboardActions = KeyboardActions(
+                                        onSearch = {
+                                            viewModel.newSearch(query = query)
+                                            // close keyboard here
+                                        },
+                                    ),
+                                    leadingIcon = {
+                                        Icon(
+                                            imageVector = Icons.Filled.Search,
+                                            contentDescription = ""
+                                        )
+                                    },
+                                    textStyle = TextStyle(color = MaterialTheme.colors.surface),
+                                    colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
+                                )
+
+                            }
                         }
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
+                    }
                     LazyColumn {
                         itemsIndexed(
                             items = recipes
